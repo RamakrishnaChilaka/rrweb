2fcbce1bee6a0e877dec8a69e7521cd1d685841d
ramakrishna chilaka <ramakrishna@nference.net>
2019-10-10 05:26:04 +0000

    update package.json to include es6 libs of react player

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,6 @@ import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
-function toRecordPath(path) {
-  return path.replace(/^([\w]+)\//, '$1/record/').replace('rrweb', 'rrweb-record');
-}
-
-function toMinPath(path) {
-  return path.replace(/\.js$/, '.min.js');
-}
-
 let configs = [
   {
     input: './src/index.ts',
@@ -31,7 +23,25 @@ let configs = [
         file: pkg.module,
       },
     ],
-  }
+  },
+  {
+    input: './src/index.ts',
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+      postcss({
+        extract: false,
+        inject: false,
+      }),
+    ],
+    output: [
+      {
+        format: 'esm',
+        file: pkg.reactPlayer,
+      },
+    ],
+  },
 ];
 
 export default configs;
